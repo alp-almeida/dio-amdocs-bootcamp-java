c4d84003ae4f5626144dd40385bc1619f03a4a6a
André Luiz Pereira de Almeida <andre_almeida15@hotmail.com>
2022-01-19 12:20:58 -0300

    Update Banco.java

--- Banco/src/Banco.java
+++ Banco/src/Banco.java
@@ -4,8 +4,8 @@ public class Banco {
 	private String nome;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Cliente cli_cc = new Cliente("André","35619464881");
-		Cliente cli_cp = new Cliente("Mirian","34163606807");
+		Cliente cli_cc = new Cliente("AndrÃ©", "90000000000");
+		Cliente cli_cp = new Cliente("Mirian","90000000001");
 		ContaCorrente cc = new ContaCorrente(cli_cc);
 		ContaPoupanca cp = new ContaPoupanca(cli_cp);
 		cc.extrato();
