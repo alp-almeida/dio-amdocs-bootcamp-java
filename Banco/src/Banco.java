47c29404ee8c60aef094aa11d270d3d21780f998
Andre Almeida <andre_almeida15@hotmail.com>
2022-01-24 09:31:06 -0300

    atualizando arquivo banco

--- Banco/src/Banco.java
+++ Banco/src/Banco.java
@@ -4,4 +4,12 @@ public class Banco {
 	private String nome;
 	List<Cliente> clientes = new ArrayList();
 
+	public void getListaClientes(){
+
+		for (Cliente c : clientes) {
+			System.out.println(c.toString());
+		}
+
+	}
+
 }
